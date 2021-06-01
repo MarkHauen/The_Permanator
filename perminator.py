b4867880697c74d73faff647181052f0fc3af8d2
Mark Hauenstein <markhauen@gmail.com>
2021-06-01 17:45:21 -0500

    Update perminator.py

--- perminator.py
+++ perminator.py
@@ -1,7 +1,6 @@
 import sys
 
 word = sys.argv[1]
-l = len(word)
 
 def applyCapPattern(word, pattern):
    wordArr = list(word)
@@ -10,11 +9,11 @@ def applyCapPattern(word, pattern):
            wordArr[i] = wordArr[i].upper()
    return ''.join(wordArr)
 
-def allPatterns():
-   return [bin(x).replace("0b", "").zfill(l) for x in range(0, int('1' * l, 2) + 1)]
+def allPatterns(w):
+   return [bin(x).replace("0b", "").zfill(len(w)) for x in range(0, int('1' * l, 2) + 1)]
 
 def main():
-   x = [applyCapPattern(word, x) for x in allPatterns()]
+   x = [applyCapPattern(word, x) for x in allPatterns(word)]
    for z in x:
        print(z)
    print(len(x))
